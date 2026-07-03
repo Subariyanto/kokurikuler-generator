cd4c28e0547123807e7523ab67afa403fe8c5557
Subariyanto <subariyantoss2@gmail.com>
2026-07-03 13:39:55 +0700

    Fix: remove basename for Netlify root deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/kokurikuler-generator">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
